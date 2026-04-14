cf2e21a111f014ad6ef109bce727745c12b0f7e1
icexuan1001 <2083404975@qq.com>
2026-04-14 23:31:35 +0800

    Initial commit

---
+++ src/main/java/com/xuan/springbootinit/common/DeleteRequest.java
@@ -0,0 +1,16 @@
+package com.xuan.springbootinit.common;
+
+import java.io.Serializable;
+import lombok.Data;
+
+
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
